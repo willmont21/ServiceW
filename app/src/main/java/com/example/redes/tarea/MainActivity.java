da0490140d07d39e73f75924c8dbf38f057302a6
DPTO DE REDES <redes@ggedclabwrk018.espol.edu.ec>
2019-01-14 07:58:59 -0500

    commit

---
+++ app/src/main/java/com/example/redes/tarea/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.redes.tarea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+
+}
